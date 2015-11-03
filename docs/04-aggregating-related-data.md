e0542987d81b7cfa97e4956133ec42108111eecc
Lal Thomas <lal.thomas.mail@gmail.com>
2015-11-03 21:57:50 +0530

    merging of excercixes and lab programs

--- docs/04-aggregating-related-data.md
+++ docs/04-aggregating-related-data.md
@@ -1,5 +1,6 @@
 Aggregating related data
 =========================
+
 Accessing positional information in lists
 Representing ordered data with tuples
 Consistently handling data collections with iterators
---
+++ excercise/02-print-days-of-week.py
@@ -0,0 +1,18 @@
+
+def print_days_of_week():
+
+	days_of_week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday' ];
+
+	for day in days_of_week:
+
+		print day
+
+def main():
+
+	print_days_of_week()
+
+
+if __name__ == '__main__':
+
+	main()
+	
