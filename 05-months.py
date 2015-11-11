0194d1a9d06e3999100b8de3add9ae7b27568a08
Lal Thomas <lal.thomas.mail@gmail.com>
2015-11-11 16:42:57 +0530

    add month count to display

--- 05-months.py
+++ 05-months.py
@@ -1,7 +1,10 @@
+'''
+program to display months and its indexes
+'''
 __author__ = 'Lal Thomas'
 __email__ = 'lal.thomas.mail@gnail.com'
 
 months = ['January', 'February' ,'March', 'April', 'May','June', 'July', 'August', 'September', 'October', 'November','December'];
 
-for month in months:
-    print(month, )
+for option,month in enumerate(months):
+    print(month,option)
